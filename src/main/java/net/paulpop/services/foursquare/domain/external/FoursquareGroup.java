1570bf5b706b43a5f242e9062fafcff9d582ac64
Paul Pop <paul.pop@betfair.com>
2015-08-14 18:38:18 +0100

    Add missing POJO

---
+++ src/main/java/net/paulpop/services/foursquare/domain/external/FoursquareGroup.java
@@ -0,0 +1,17 @@
+package net.paulpop.services.foursquare.domain.external;
+
+import java.util.List;
+
+/**
+ * Created by popp on 14/08/15.
+ */
+@SuppressWarnings("unused")
+public final class FoursquareGroup {
+
+    private List<FoursquareItem> items;
+
+    public List<FoursquareItem> getItems() {
+        return items;
+    }
+
+}
